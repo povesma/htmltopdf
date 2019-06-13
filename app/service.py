b9ad4fc5e3e4e448bcdc62586e2a731aa0d21460
Dmytro Povesma <povesma@gmail.com>
2019-06-13 12:49:25 +0300

    File names and dirs improvement

--- app/service.py
+++ app/service.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, send_from_directory, send_file
 import json, uuid, subprocess
+import os
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(__name__, static_url_path = '')
 
 FILES_DIR = 'files'
 INPUT_HTML_DIR = 'input_html'
+
+pdfs_path = '{}/{}'.format(ROOT_PATH, FILES_DIR)
+htmls_path = '{}/{}'.format(ROOT_PATH, INPUT_HTML_DIR)
 
 @app.route('/')
 def home():
@@ -16,20 +23,19 @@ def pdf():
 	data = request.get_json()
 
 	temp_name = uuid.uuid4()
-	file_path = '/app/{}/{}.pdf'.format(FILES_DIR, temp_name)
+	file_path = '{}/{}.pdf'.format(pdfs_path, temp_name)
 	# _format = data.get('format') or ['url'] # just URL by default
-	file = None
+	source = data.get('url')
 	html = data.get('html')
 	if (html):
 		# Create a file
-		filename = "{}/{}.html".format(INPUT_HTML_DIR, temp_name)
+		filename = "{}/{}.html".format(htmls_path, temp_name)
 		_f = open(filename, 'w+')
 		_f.write(html)
 		_f.close()
 		# set source as a filename
 		source = filename
-	source = data.get('url') or file
-	pdf_generated = subprocess.call(['wkhtmltopdf', source, temp_path])
+	pdf_generated = subprocess.call(['wkhtmltopdf', source, file_path])
 	
 	if(pdf_generated == 0):
 		resp = {
@@ -39,7 +45,8 @@ def pdf():
 	else :
 		return (json.dumps({"error": "pdf generation failed"}), 500, {"Content-Type":"application/json"})
 
-@app.route('/files/<uuid:path>', methods=['GET'])
+@app.route('/files/<string:path>', methods=['GET'])
 def send_pdf(path):
+	#fn = "{}.{}".format(path, '.pdf')
 	print (path)
-	return send_from_directory(FILES_DIR, path)
+	return send_from_directory(pdfs_path, path)
