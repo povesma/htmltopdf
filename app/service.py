533ea0be8428909b62039bb1d567940f509d0753
Dmytro Povesma <povesma@gmail.com>
2019-06-13 11:59:14 +0300

    Jsonified errors; 'html' field is converted to PDF (no 'url' needed)'

--- app/service.py
+++ app/service.py
@@ -1,30 +1,45 @@
-from flask import Flask, request, send_from_directory,send_file
-import json,uuid,subprocess
-app = Flask(__name__,static_url_path='')
+from flask import Flask, request, send_from_directory, send_file
+import json, uuid, subprocess
+app = Flask(__name__, static_url_path = '')
+
+FILES_DIR = 'files'
+INPUT_HTML_DIR = 'input_html'
 
 @app.route('/')
 def home():
     return json.dumps({
-		"response":"online"
+		"response": "online"
 	})
 
-@app.route('/pdf',methods=['POST'])
+@app.route('/pdf', methods=['POST'])
 def pdf():
 	data = request.get_json()
 
 	temp_name = uuid.uuid4()
-	temp_path = '/app/temp/{}.pdf'.format(temp_name)
-	pdf_generated = subprocess.call(['wkhtmltopdf',data['url'],temp_path])
+	file_path = '/app/{}/{}.pdf'.format(FILES_DIR, temp_name)
+	# _format = data.get('format') or ['url'] # just URL by default
+	file = None
+	_html = data.get('html')
+	if (html):
+		# Create a file
+		filename = INPUT_HTML_DIR + '/' + temp_name
+		_f = open(filename, 'w+')
+		_f.write(html)
+		_f.close()
+		# set source as a filename
+		source = filename
+	source = data.get('url') or file
+	pdf_generated = subprocess.call(['wkhtmltopdf', source, temp_path])
 	
 	if(pdf_generated == 0):
 		resp = {
-			"file":"/temp/{}.pdf".format(temp_name)
+			"file":"/{}/{}.pdf".format(FILES_DIR, temp_name)
 		}
-		return (json.dumps(resp),200,{"Content-Type":"application/json"})
+		return (json.dumps(resp), 200, {"Content-Type":"application/json"})
 	else :
-		return json.dumps({ "error": "pdf generation failed"})
+		return (json.dumps({"error": "pdf generation failed"}), 500, {"Content-Type":"application/json"})
 
-@app.route('/temp/<path:path>')
-def send_js(path):
+@app.route('/files/<uuid:path>', methods=['GET'])
+def send_pdf(path):
 	print (path)
-	return send_from_directory('temp', path)
+	return send_from_directory(FILES_DIR, path)
